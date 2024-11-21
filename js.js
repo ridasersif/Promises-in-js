6b8c9bca08741f470cebfde738d1e6323a73b425
ridasersif <ridasersif1@gmail.com>
2024-11-21 08:09:26 +0100

    telecharge opm de Axios

---
+++ js.js
@@ -0,0 +1,27 @@
+function getUsersUsingAxios(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+        console.log(response)
+        document.getElementById('users').innerHTML=""
+        for(user of response.data){
+             users.innerText+=`
+          
+           ${user.name}
+            ${user.email}
+           
+            `
+        }
+
+    })
+}
+
+function getPostesAxios(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((response)=>{
+        console.log(response)
+    })
+}
+getPostesAxios()
+getUsersUsingAxios()
+
+
